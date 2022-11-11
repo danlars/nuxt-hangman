8ab2d8d71acf1ef6d023eb940c7ce30ed11f3e24
Daniel Joachim Larsen <daniellarsen725@gmail.com>
2022-11-11 14:08:03 +0100

    Add head meta data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,19 @@
 export default defineNuxtConfig({
     app: {
         head: {
-            title: 'Nuxt Hangman'
+            title: 'Nuxt Hangman',
+            htmlAttrs: {
+                lang: 'en',
+            },
+            meta: [
+                { charset: 'utf-8' },
+                { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+                {
+                  hid: 'description',
+                  name: 'description',
+                  content: 'Nuxt Hangman',
+                }
+              ],
         }
     }
 })
